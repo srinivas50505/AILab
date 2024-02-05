4d7791aa107c1725a2bd246709db0ed2ccdeceef
Srinivasa Pavan <82796461+srinivas50505@users.noreply.github.com>
2024-02-05 20:49:17 +0530

    Create forward_chaining.py

---
+++ forward_chaining.py
@@ -0,0 +1,24 @@
+global facts
+global is_changed
+
+is_changed = True
+facts = [["vertebrate","duck"],["flying","duck"],["mammal","cat"]]
+
+def assert_fact(fact):
+    global facts
+    global is_changed
+    if not fact in facts:
+        facts += [fact]
+        is_changed = True
+
+while is_changed:
+    is_changed = False
+    for A1 in facts:
+        if A1[0] == "mammal":
+            assert_fact(["vertebrate",A1[1]])
+        if A1[0] == "vertebrate":
+            assert_fact(["animal",A1[1]])
+        if A1[0] == "vertebrate" and ["flying",A1[1]] in facts:
+            assert_fact(["bird",A1[1]])
+
+print(facts)
